3337bd22a49772273f616b9b26220fe0f2457f93
Your Name <you@example.com>
2017-06-02 13:36:17 -0600

    With class

--- app5-queries_params/index.js
+++ app5-queries_params/index.js
@@ -7,9 +7,29 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/api/data', function (req, res) {
+	let winners = data.filter(function (e, i, arr) {
+		let q = req.query
+		for (let key in q) {
+			if(e[key] !== q[key]){
+				return false
+			}
+		}
+		return true
+	})
+	res.status(200).send(winners)
+})
 
+app.post('/api/data', function (req, res) {
+	data.push(req.body)
+	res.status(200).send('ok')
+})
 
+app.get('/api/data/:id', function (req, res) {
+	res.status(200).send(data[req.params.id])
 
-app.listen(port, function() {
-	console.log('Listening on port',port);
+})
+
+app.listen(port, function () {
+	console.log('Listening on port' + port);
 })
